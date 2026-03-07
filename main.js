ab3a845b4e6161fa06453023a5c52c6c65d8abea
Abombidus <abombidus@gmail.com>
2026-03-07 13:10:56 +0100

    Final0

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
-const commander = document.getElementById("commander")
+// const commander = document.getElementById("commander")
 const counter = document.querySelector(".counter")
 const btnHome = document.getElementById("home")
-const btnCold = document.getElementById("cold")
-const btnHot = document.getElementById("hot")
-const path = require("path")
+const funcAdd = document.querySelectorAll(".funcAdd")
+// const btnCold = document.getElementById("cold")
+// const btnHot = document.getElementById("hot")
+// const path = require("path")
 
-const diractory = path(__direname, 'home.html')
+// const diractory = path(__direname, 'home.html')
 
 
-btnHome.addEventListener("click", (e) => {
-    e.target ? directory : ""
-})
 
 let increment = 0
 
+funcAdd.addEventListener("click", () => {
+    increment += 1
+    counter.textContent = `+ ${increment}`
+})
+
 function ajouter() {
     increment++
-    counter.textContent = increment
+    counter.textContent = `+ ${increment}`
 }
 
 function acheter() {
